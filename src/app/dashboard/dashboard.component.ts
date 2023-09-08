0d70e5a8ca0b7b248849073e8efd79230002f693
Hramosm1 <hectorzramos@gmail.com>
2023-09-08 16:52:38 -0600

    Se agrega configuracion de graficas

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, inject } from '@angular/core';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 import { map } from 'rxjs/operators';
 import { Router } from '@angular/router';
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-dashboard',
@@ -11,13 +13,18 @@ import { Router } from '@angular/router';
 export class DashboardComponent implements OnInit{
   private breakpointObserver = inject(BreakpointObserver);
   router = new Router();
-  /** Based on the screen size, switch from standard to one column per row */
+  public chart: any;
+
+  constructor(){}
+
   ngOnInit(): void {
     const token = localStorage.getItem('token');
 
     if (!token) {
       this.router.navigate(['login']);
     }
+
+    this.createCharts();
   }
 
   cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
@@ -39,4 +46,68 @@ export class DashboardComponent implements OnInit{
       ];
     })
   );
+
+  createCharts(): void {
+    const xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
+    const yValues = [55, 49, 44, 24, 15];
+    const barColors = [
+      "#b91d47",
+      "#00aba9",
+      "#2b5797",
+      "#e8c3b9",
+      "#1e7145"
+    ];
+
+    const myChart1 = new Chart("myChart", {
+      type: 'bar',
+      data: {
+          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+          datasets: [{
+              label: '# usuarios',
+              data: [12, 19, 3, 5, 2, 3],
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              y: {
+                  beginAtZero: true
+              }
+          }
+      }
+  });
+
+  const myChart2 = new Chart("myChart2", {
+    type: "pie",
+    data: {
+      labels: xValues,
+      datasets: [{
+        backgroundColor: barColors,
+        data: yValues
+      }]
+    },
+    options: {
+
+    }
+  });
+
+
+  }
+
 }
