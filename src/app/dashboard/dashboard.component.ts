825e165e3b59124ed0c6e87de480f77a6cf10577
Hramosm1 <hectorzramos@gmail.com>
2023-09-09 17:28:25 -0600

    Configuracion de graficas

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -48,20 +48,14 @@ export class DashboardComponent implements OnInit{
   );
 
   createCharts(): void {
-    const xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
-    const yValues = [55, 49, 44, 24, 15];
-    const barColors = [
-      "#b91d47",
-      "#00aba9",
-      "#2b5797",
-      "#e8c3b9",
-      "#1e7145"
-    ];
+    const xValues = ['Multiperfiles', 'Bayer', 'Unipharm', 'Pepsi', 'Toledo'];
+    const yValues = [12000, 12500, 12500, 3000, 1000];
+    const xValuesM = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'];
 
-    const myChart1 = new Chart("myChart", {
+    const myChart = new Chart("myChart", {
       type: 'bar',
       data: {
-          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+          labels: ["Multiperfiles", "Bayer", "Unipharm", "Pepsi", "Toledo"],
           datasets: [{
               label: '# usuarios',
               data: [12, 19, 3, 5, 2, 3],
@@ -89,25 +83,60 @@ export class DashboardComponent implements OnInit{
               y: {
                   beginAtZero: true
               }
+          },
+          plugins: {
+            legend: {display: false}
           }
       }
-  });
+    });
 
-  const myChart2 = new Chart("myChart2", {
-    type: "pie",
-    data: {
-      labels: xValues,
-      datasets: [{
-        backgroundColor: barColors,
-        data: yValues
-      }]
-    },
-    options: {
+    const myChart2 = new Chart("myChart2", {
+      type: "line",
+      data: {
+        labels: xValues,
+        datasets: [{
+          backgroundColor:"rgba(0,0,255,1.0)",
+          borderColor: "rgba(0,0,255,0.1)",
+          data: yValues
+        }]
+      },
+      options:{
+        scales: {
+          y: {
+              beginAtZero: true
+          }
+        },
+        plugins: {
+          legend: {display: false}
+        }
+      }
+    });
 
-    }
-  });
+    const myChart3 = new Chart("myChart3", {
+      type: "line",
+      data: {
+        labels: xValuesM,
+        datasets: [{
+          data: [860,1140,1060,1060,1070,1110,1330,2210,7830,2478],
+          borderColor: "red",
+          fill: false
+        },{
+          data: [1600,1700,1700,1900,2000,2700,4000,5000,6000,7000],
+          borderColor: "green",
+          fill: false
+        },{
+          data: [300,700,2000,5000,6000,4000,2000,1000,200,100],
+          borderColor: "blue",
+          fill: false
+        }]
+      },
+      options: {
+        plugins: {
+          legend: {display: false}
+        }
+      }
+    });
 
 
   }
-
 }
